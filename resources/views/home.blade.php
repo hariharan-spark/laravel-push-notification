e608960fdf0c8388944e9a120508f9f5e1ba98a0
hariharan-spark <mailto:hariharan@sparkouttech.com>
2023-02-23 14:53:25 +0530

    Send Web Push Notification with Firebase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+   
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <center>
+                <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
+            </center>
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+  
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+  
+                    <form action="{{ route('send.notification') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label>Body</label>
+                            <textarea class="form-control" name="body"></textarea>
+                          </div>
+                        <button type="submit" class="btn btn-primary">Send Notification</button>
+                    </form>
+  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
+<script>
+  
+    var firebaseConfig = {
+        apiKey: "AIzaSyATjGZsscEIzj0SaFMzcepaYOcBI6-XSWU",
+        authDomain: "test-notification-app-bdfb3.firebaseapp.com",
+        databaseURL: "https://XXXX.firebaseio.com",
+        projectId: "test-notification-app-bdfb3",
+        storageBucket: "test-notification-app-bdfb3.appspot.com",
+        messagingSenderId: "896281978896",
+        appId: "1:896281978896:web:fbec745f8bf60c78e28295",
+        measurementId: "G-W4ETNE60L6"
+    };      
+    firebase.initializeApp(firebaseConfig);
+    const messaging = firebase.messaging();
+  
+    function initFirebaseMessagingRegistration() {
+            messaging
+            .requestPermission()
+            .then(function () {
+                return messaging.getToken()
+            })
+            .then(function(token) {
+                console.log(token);
+   
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+  
+                $.ajax({
+                    url: '{{ route("save-token") }}',
+                    type: 'POST',
+                    data: {
+                        token: token
+                    },
+                    dataType: 'JSON',
+                    success: function (response) {
+                        alert('Token saved successfully.');
+                    },
+                    error: function (err) {
+                        alert(2);
+                        
+                        console.log('User Chat Token Error'+ err);
+                    },
+                });
+  
+            }).catch(function (err) {
+                console.log('User Chat Token Error'+ err);
+            });
+     }
+      
+    messaging.onMessage(function(payload) {
+        const noteTitle = payload.notification.title;
+        const noteOptions = {
+            body: payload.notification.body,
+            icon: payload.notification.icon,
+        };
+        new Notification(noteTitle, noteOptions);
+    });
+   
+</script>
+@endsection
